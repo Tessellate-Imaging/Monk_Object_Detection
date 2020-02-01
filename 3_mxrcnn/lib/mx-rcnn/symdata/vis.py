9023d88bbccca47bd073c84ca43a995540095137
Abhishek Kumar Annamraju <abhishek4273@gmail.com>
2020-02-01 19:28:46 +0530

    Updated visualizer to save figure

--- 3_mxrcnn/lib/mx-rcnn/symdata/vis.py
+++ 3_mxrcnn/lib/mx-rcnn/symdata/vis.py
@@ -12,4 +12,5 @@ def vis_detection(im_orig, detections, class_names, thresh=0.7):
             plt.gca().add_patch(rect)
             plt.gca().text(x1, y1 - 2, '{:s} {:.3f}'.format(class_names[cls], conf),
                            bbox=dict(facecolor=colors[cls], alpha=0.5), fontsize=12, color='white')
+    plt.save("output.png")
     plt.show()
