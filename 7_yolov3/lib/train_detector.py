fb825b86680096431763d85d4a956fc9be2c8277
Abhishek Kumar Annamraju <abhishek4273@gmail.com>
2020-02-02 23:13:27 +0530

    Updated tqdm import to auto-select environment

--- 7_yolov3/lib/train_detector.py
+++ 7_yolov3/lib/train_detector.py
@@ -13,7 +13,23 @@ from utils.datasets import *
 from utils.utils import *
 
 from torch.utils.tensorboard import SummaryWriter
-from tqdm.notebook import tqdm
+
+def isnotebook():
+    try:
+        shell = get_ipython().__class__.__name__
+        if shell == 'ZMQInteractiveShell':
+            return True   # Jupyter notebook or qtconsole
+        elif shell == 'TerminalInteractiveShell':
+            return False  # Terminal running IPython
+        else:
+            return False  # Other type (?)
+    except NameError:
+        return False
+if(isnotebook()):
+    from tqdm.notebook import tqdm
+else:
+    from tqdm import tqdm as tqdm
+
 from update_cfg import update
 from apex import amp
 
