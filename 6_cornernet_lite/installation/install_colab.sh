b0c9ed5919f250e53baf61fedfc4430aa75348e5
abhi-kumar <abhishek4273@gmail.com>
2020-03-28 23:28:43 +0530

    Added colab installation for 6_cornernet_lite - part 3

--- 6_cornernet_lite/installation/install_colab.sh
+++ 6_cornernet_lite/installation/install_colab.sh
@@ -1,5 +1,5 @@
 cat requirements_colab.txt | xargs -n 1 -L 1 pip install
 
-cd ../lib/core/models/py_utils/_cpools/ && pip install -e .
+cd ../lib/core/models/py_utils/_cpools/ && python setup.py install --user
 
 cd ../../../external && make
