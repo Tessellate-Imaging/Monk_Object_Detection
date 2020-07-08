5ee2226e72a9ab1eb5141fbea9c856d41f6ccb07
abhi-kumar <abhishek4273@gmail.com>
2020-07-08 10:09:11 +0530

    Resolving error - virtualenvwrapper.sh not found

--- inference_engine/gluoncv_finetune/lib/infer_folder_img_ssd.sh
+++ inference_engine/gluoncv_finetune/lib/infer_folder_img_ssd.sh
@@ -1,6 +1,8 @@
 export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3.6
 export WORKON_HOME=$HOME/.virtualenvs
 . $HOME/.local/bin/virtualenvwrapper.sh
+. /usr/local/bin/virtualenvwrapper.sh
+
 
 export MXNET_CUDNN_AUTOTUNE_DEFAULT=0
 workon gluoncv_finetune && python Monk_Object_Detection/inference_engine/gluoncv_finetune/lib/infer_folder_img_ssd.py
