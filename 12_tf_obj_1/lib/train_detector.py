9c20cb3586fc97b4c9323d2a6e2e6e4051550dbd
abhi-kumar <abhishek4273@gmail.com>
2020-08-09 21:08:28 +0530

    Added new model support to 12_obj_tf_1 rfcn_resnet101

--- 12_tf_obj_1/lib/train_detector.py
+++ 12_tf_obj_1/lib/train_detector.py
@@ -93,6 +93,12 @@ class Detector():
                 os.system("wget http://download.tensorflow.org/models/object_detection/faster_rcnn_resnet50_lowproposals_coco_2018_01_28.tar.gz");
                 os.system("tar -xvzf faster_rcnn_resnet50_lowproposals_coco_2018_01_28.tar.gz");
             return model_name;
+        elif(model_name == "rfcn_resnet101"):
+            model_name = "rfcn_resnet101_coco_2018_01_28";
+            if(not os.path.isdir(model_name)):
+                os.system("wget http://download.tensorflow.org/models/object_detection/rfcn_resnet101_coco_2018_01_28.tar.gz");
+                os.system("tar -xvzf rfcn_resnet101_coco_2018_01_28.tar.gz");
+            return model_name;
         
     
     
@@ -643,6 +649,52 @@ class Detector():
 
             with open(model_name + "/pipeline_updated.config", 'w') as file_handler:
                 file_handler.writelines(lines)
+                
+        elif(model_name == "rfcn_resnet101_coco_2018_01_28"):
+            f = open(model_name + "/pipeline.config");
+            data = f.read();
+            f.close();
+
+            data = data.replace("num_classes: 90", 
+                                "num_classes: " + str(num_classes));
+            data = data.replace("initial_learning_rate: 0.000300000014249", 
+                                "initial_learning_rate: " + str(lr));
+            
+            data = data.replace("step: 0", 
+                                "step: " + str(1));
+            data = data.replace("learning_rate: 0.000300000014249", 
+                                "learning_rate: " + str(lr));
+            
+            data = data.replace("step: 900000", 
+                                "step: " + str(num_steps//3));
+            data = data.replace("learning_rate: 2.99999992421e-05", 
+                                "learning_rate: " + str(lr/10));
+                                
+            data = data.replace("step: 1200000", 
+                                "step: " + str(2*num_steps//3));
+            data = data.replace("learning_rate: 3.00000010611e-06", 
+                                "learning_rate: " + str(lr/100));                    
+
+
+            data = data.replace("fine_tune_checkpoint: \"PATH_TO_BE_CONFIGURED/model.ckpt\"", 
+                                "fine_tune_checkpoint: \"" + model_name + "/model.ckpt\"");
+
+            data = data.replace("label_map_path: \"PATH_TO_BE_CONFIGURED/mscoco_label_map.pbtxt\"", 
+                                "label_map_path: \"" + label_map + "\"");
+
+            data = data.replace("input_path: \"PATH_TO_BE_CONFIGURED/mscoco_train.record\"", 
+                                "input_path: \"" + output_path + "/train.record\"");
+
+            data = data.replace("input_path: \"PATH_TO_BE_CONFIGURED/mscoco_val.record\"", 
+                                "input_path: \"" + output_path + "/val.record\"");
+
+            data = data.replace("batch_size: 1", 
+                                "batch_size: " + str(batch_size));
+            
+            
+            f = open(model_name + "/pipeline_updated.config", 'w');
+            f.write(data);
+            f.close()
     
          
     
@@ -654,7 +706,8 @@ class Detector():
                                           "ssd_mobilenet_v1_quantized", "ssd_mobilenet_v1_0.75_depth_quantized",
                                           "ssd_mobilenet_v2_quantized", "ssdlite_mobilenet_v2",
                                           "ssd_inception_v2", "faster_rcnn_inception_v2",
-                                          "faster_rcnn_resnet50", "faster_rcnn_resnet50_lowproposals"
+                                          "faster_rcnn_resnet50", "faster_rcnn_resnet50_lowproposals",
+                                          "rfcn_resnet101"
                                          ];
         for i in range(len(self.system_dict["model_list"])):
             print("{}. Model Name: {}".format(i+1, self.system_dict["model_list"][i]));
@@ -755,7 +808,8 @@ class Detector():
             self.system_dict["input_shape"] = "-1, 640, 640, 3";
         elif(self.system_dict["model_name"] == "faster_rcnn_inception_v2_coco_2018_01_28" or
             self.system_dict["model_name"] == "faster_rcnn_resnet50_coco_2018_01_28" or
-            self.system_dict["model_name"] == "faster_rcnn_resnet50_lowproposals_coco_2018_01_28"):
+            self.system_dict["model_name"] == "faster_rcnn_resnet50_lowproposals_coco_2018_01_28" or
+            self.system_dict["model_name"] == "rfcn_resnet101_coco_2018_01_28"):
             self.system_dict["input_shape"] = None; 
             
         
