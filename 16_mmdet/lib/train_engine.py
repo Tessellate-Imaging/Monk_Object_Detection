6c9befe69905e0646d2a051e5adb03ad37351ae0
abhi-kumar <abhishek4273@gmail.com>
2020-08-23 12:01:57 +0530

    Added cascade rcnn support to 16_mmdet pipeline

--- 16_mmdet/lib/train_engine.py
+++ 16_mmdet/lib/train_engine.py
@@ -61,7 +61,9 @@ class Detector():
         
     def List_Models(self):
         self.system_dict["params"]["model_list"] = ["faster_rcnn_fpn50", "faster_rcnn_fpn101", 
-                                                    "faster_rcnn_x101_32x4d_fpn", "faster_rcnn_x101_64x4d_fpn"]
+                                                    "faster_rcnn_x101_32x4d_fpn", "faster_rcnn_x101_64x4d_fpn",
+                                                    "cascade_rcnn_fpn50", "cascade_rcnn_fpn101", 
+                                                    "cascade_rcnn_x101_32x4d_fpn", "cascade_rcnn_x101_64x4d_fpn"]
         for i in range(len(self.system_dict["params"]["model_list"])):
             print("{}. Model - {}".format(i+1, self.system_dict["params"]["model_list"][i]));
             
@@ -80,8 +82,21 @@ class Detector():
         elif(model_name == "faster_rcnn_x101_64x4d_fpn"):
             self.system_dict["local"]["model_name"] = "faster_rcnn_x101_64x4d_fpn_coco";
             self.system_dict["params"]["load_from"] = "https://open-mmlab.s3.ap-northeast-2.amazonaws.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_x101_64x4d_fpn_1x_coco/faster_rcnn_x101_64x4d_fpn_1x_coco_20200204-833ee192.pth";
-            
+        elif(model_name == "cascade_rcnn_fpn50"):
+            self.system_dict["local"]["model_name"] = "cascade_rcnn_fpn50_coco";
+            self.system_dict["params"]["load_from"] = "https://open-mmlab.s3.ap-northeast-2.amazonaws.com/mmdetection/v2.0/cascade_rcnn/cascade_rcnn_r50_fpn_1x_coco/cascade_rcnn_r50_fpn_1x_coco_20200316-3dc56deb.pth";  
+        elif(model_name == "cascade_rcnn_fpn101"):
+            self.system_dict["local"]["model_name"] = "cascade_rcnn_fpn101_coco";
+            self.system_dict["params"]["load_from"] = "https://open-mmlab.s3.ap-northeast-2.amazonaws.com/mmdetection/v2.0/cascade_rcnn/cascade_rcnn_r101_fpn_1x_coco/cascade_rcnn_r101_fpn_1x_coco_20200317-0b6a2fbf.pth"; 
+        elif(model_name == "cascade_rcnn_x101_32x4d_fpn"):
+            self.system_dict["local"]["model_name"] = "cascade_rcnn_x101_32x4d_fpn_coco";
+            self.system_dict["params"]["load_from"] = "https://open-mmlab.s3.ap-northeast-2.amazonaws.com/mmdetection/v2.0/cascade_rcnn/cascade_rcnn_x101_32x4d_fpn_1x_coco/cascade_rcnn_x101_32x4d_fpn_1x_coco_20200316-95c2deb6.pth"; 
+        elif(model_name == "cascade_rcnn_x101_64x4d_fpn"):
+            self.system_dict["local"]["model_name"] = "cascade_rcnn_x101_64x4d_fpn_coco";
+            self.system_dict["params"]["load_from"] = "https://open-mmlab.s3.ap-northeast-2.amazonaws.com/mmdetection/v2.0/cascade_rcnn/cascade_rcnn_x101_64x4d_fpn_1x_coco/cascade_rcnn_x101_64x4d_fpn_1x_coco_20200515_075702-43ce6a30.pth"; 
         
+    
+    
           
     def Hyper_Params(self, lr=0.02, momentum=0.9, weight_decay=0.0001):
         self.system_dict["params"]["lr"] = lr;
@@ -295,11 +310,211 @@ class Detector():
             f = open("config_updated.py", 'w');
             f.write(lines);
             f.close();
+        elif(self.system_dict["local"]["model_name"] == "cascade_rcnn_fpn50_coco"):
+            f = open("Monk_Object_Detection/16_mmdet/lib/cfgs/cascade_rcnn_fpn50_coco.py");
+            lines = f.read();
+            f.close();
+            
+            lines = lines.replace("samples_per_gpu=",
+                                  "samples_per_gpu=" + str(self.system_dict["params"]["batch_size"]));
+            lines = lines.replace("workers_per_gpu=",
+                                  "workers_per_gpu=" + str(self.system_dict["params"]["num_workers"]));
+            lines = lines.replace("ann_file=,",
+                                  "ann_file='" + str(self.system_dict["params"]["train_anno_file"]) + "',", 1);
+            lines = lines.replace("ann_file=,",
+                                  "ann_file='" + str(self.system_dict["params"]["val_anno_file"]) + "',", 2);
+            lines = lines.replace("ann_file=,",
+                                  "ann_file='" + str(self.system_dict["params"]["val_anno_file"]) + "',", 3);
+            lines = lines.replace("img_prefix=,",
+                                  "img_prefix='" + str(self.system_dict["params"]["train_img_folder"]) + "/',", 1);
+            lines = lines.replace("img_prefix=,",
+                                  "img_prefix='" + str(self.system_dict["params"]["val_img_folder"]) + "/',", 2);
+            lines = lines.replace("img_prefix=,",
+                                  "img_prefix='" + str(self.system_dict["params"]["val_img_folder"]) + "/',", 3);
+            lines = lines.replace("evaluation = dict(interval=",
+                                  "evaluation = dict(interval=" + str(self.system_dict["params"]["val_interval"]));
+            lines = lines.replace("checkpoint_config = dict(interval=",
+                                  "checkpoint_config = dict(interval=" + str(self.system_dict["params"]["val_interval"]));
+            lines = lines.replace("lr=",
+                                  "lr=" + str(self.system_dict["params"]["lr"]));
+            lines = lines.replace("momentum=",
+                                  "momentum=" + str(self.system_dict["params"]["momentum"]));
+            lines = lines.replace("weight_decay=",
+                                  "weight_decay=" + str(self.system_dict["params"]["weight_decay"]));
+            lines = lines.replace("total_epochs =",
+                                  "total_epochs =" + str(self.system_dict["params"]["num_epochs"]));
+            lines = lines.replace("load_from =",
+                                  "load_from = '" + str(self.system_dict["params"]["load_from"]) + "'");
+            lines = lines.replace("classes=,",
+                                  "classes=" + str(self.system_dict["params"]["classes"]) + ",");
+            lines = lines.replace("num_classes=80",
+                                  "num_classes=" + str(len(self.system_dict["params"]["classes"])));
+
+            if(self.system_dict["params"]["num_epochs"] >= 3):
+                steps = [self.system_dict["params"]["num_epochs"]//3, 2*self.system_dict["params"]["num_epochs"]//3];
+            else:
+                steps = [1];
+            lines = lines.replace("step=",
+                                  "step=" + str(steps));
+
+            f = open("config_updated.py", 'w');
+            f.write(lines);
+            f.close();
+        elif(self.system_dict["local"]["model_name"] == "cascade_rcnn_fpn101_coco"):
+            f = open("Monk_Object_Detection/16_mmdet/lib/cfgs/cascade_rcnn_fpn101_coco.py");
+            lines = f.read();
+            f.close();
+            
+            lines = lines.replace("samples_per_gpu=",
+                                  "samples_per_gpu=" + str(self.system_dict["params"]["batch_size"]));
+            lines = lines.replace("workers_per_gpu=",
+                                  "workers_per_gpu=" + str(self.system_dict["params"]["num_workers"]));
+            lines = lines.replace("ann_file=,",
+                                  "ann_file='" + str(self.system_dict["params"]["train_anno_file"]) + "',", 1);
+            lines = lines.replace("ann_file=,",
+                                  "ann_file='" + str(self.system_dict["params"]["val_anno_file"]) + "',", 2);
+            lines = lines.replace("ann_file=,",
+                                  "ann_file='" + str(self.system_dict["params"]["val_anno_file"]) + "',", 3);
+            lines = lines.replace("img_prefix=,",
+                                  "img_prefix='" + str(self.system_dict["params"]["train_img_folder"]) + "/',", 1);
+            lines = lines.replace("img_prefix=,",
+                                  "img_prefix='" + str(self.system_dict["params"]["val_img_folder"]) + "/',", 2);
+            lines = lines.replace("img_prefix=,",
+                                  "img_prefix='" + str(self.system_dict["params"]["val_img_folder"]) + "/',", 3);
+            lines = lines.replace("evaluation = dict(interval=",
+                                  "evaluation = dict(interval=" + str(self.system_dict["params"]["val_interval"]));
+            lines = lines.replace("checkpoint_config = dict(interval=",
+                                  "checkpoint_config = dict(interval=" + str(self.system_dict["params"]["val_interval"]));
+            lines = lines.replace("lr=",
+                                  "lr=" + str(self.system_dict["params"]["lr"]));
+            lines = lines.replace("momentum=",
+                                  "momentum=" + str(self.system_dict["params"]["momentum"]));
+            lines = lines.replace("weight_decay=",
+                                  "weight_decay=" + str(self.system_dict["params"]["weight_decay"]));
+            lines = lines.replace("total_epochs =",
+                                  "total_epochs =" + str(self.system_dict["params"]["num_epochs"]));
+            lines = lines.replace("load_from =",
+                                  "load_from = '" + str(self.system_dict["params"]["load_from"]) + "'");
+            lines = lines.replace("classes=,",
+                                  "classes=" + str(self.system_dict["params"]["classes"]) + ",");
+            lines = lines.replace("num_classes=80",
+                                  "num_classes=" + str(len(self.system_dict["params"]["classes"])));
+
+            if(self.system_dict["params"]["num_epochs"] >= 3):
+                steps = [self.system_dict["params"]["num_epochs"]//3, 2*self.system_dict["params"]["num_epochs"]//3];
+            else:
+                steps = [1];
+            lines = lines.replace("step=",
+                                  "step=" + str(steps));
+
+            f = open("config_updated.py", 'w');
+            f.write(lines);
+            f.close();
+        elif(self.system_dict["local"]["model_name"] == "cascade_rcnn_x101_32x4d_fpn_coco"):
+            f = open("Monk_Object_Detection/16_mmdet/lib/cfgs/cascade_rcnn_x101_32x4d_fpn_coco.py");
+            lines = f.read();
+            f.close();
+            
+            lines = lines.replace("samples_per_gpu=",
+                                  "samples_per_gpu=" + str(self.system_dict["params"]["batch_size"]));
+            lines = lines.replace("workers_per_gpu=",
+                                  "workers_per_gpu=" + str(self.system_dict["params"]["num_workers"]));
+            lines = lines.replace("ann_file=,",
+                                  "ann_file='" + str(self.system_dict["params"]["train_anno_file"]) + "',", 1);
+            lines = lines.replace("ann_file=,",
+                                  "ann_file='" + str(self.system_dict["params"]["val_anno_file"]) + "',", 2);
+            lines = lines.replace("ann_file=,",
+                                  "ann_file='" + str(self.system_dict["params"]["val_anno_file"]) + "',", 3);
+            lines = lines.replace("img_prefix=,",
+                                  "img_prefix='" + str(self.system_dict["params"]["train_img_folder"]) + "/',", 1);
+            lines = lines.replace("img_prefix=,",
+                                  "img_prefix='" + str(self.system_dict["params"]["val_img_folder"]) + "/',", 2);
+            lines = lines.replace("img_prefix=,",
+                                  "img_prefix='" + str(self.system_dict["params"]["val_img_folder"]) + "/',", 3);
+            lines = lines.replace("evaluation = dict(interval=",
+                                  "evaluation = dict(interval=" + str(self.system_dict["params"]["val_interval"]));
+            lines = lines.replace("checkpoint_config = dict(interval=",
+                                  "checkpoint_config = dict(interval=" + str(self.system_dict["params"]["val_interval"]));
+            lines = lines.replace("lr=",
+                                  "lr=" + str(self.system_dict["params"]["lr"]));
+            lines = lines.replace("momentum=",
+                                  "momentum=" + str(self.system_dict["params"]["momentum"]));
+            lines = lines.replace("weight_decay=",
+                                  "weight_decay=" + str(self.system_dict["params"]["weight_decay"]));
+            lines = lines.replace("total_epochs =",
+                                  "total_epochs =" + str(self.system_dict["params"]["num_epochs"]));
+            lines = lines.replace("load_from =",
+                                  "load_from = '" + str(self.system_dict["params"]["load_from"]) + "'");
+            lines = lines.replace("classes=,",
+                                  "classes=" + str(self.system_dict["params"]["classes"]) + ",");
+            lines = lines.replace("num_classes=80",
+                                  "num_classes=" + str(len(self.system_dict["params"]["classes"])));
+
+            if(self.system_dict["params"]["num_epochs"] >= 3):
+                steps = [self.system_dict["params"]["num_epochs"]//3, 2*self.system_dict["params"]["num_epochs"]//3];
+            else:
+                steps = [1];
+            lines = lines.replace("step=",
+                                  "step=" + str(steps));
+
+            f = open("config_updated.py", 'w');
+            f.write(lines);
+            f.close();
+        elif(self.system_dict["local"]["model_name"] == "cascade_rcnn_x101_64x4d_fpn_coco"):
+            f = open("Monk_Object_Detection/16_mmdet/lib/cfgs/cascade_rcnn_x101_64x4d_fpn_coco.py");
+            lines = f.read();
+            f.close();
+            
+            lines = lines.replace("samples_per_gpu=",
+                                  "samples_per_gpu=" + str(self.system_dict["params"]["batch_size"]));
+            lines = lines.replace("workers_per_gpu=",
+                                  "workers_per_gpu=" + str(self.system_dict["params"]["num_workers"]));
+            lines = lines.replace("ann_file=,",
+                                  "ann_file='" + str(self.system_dict["params"]["train_anno_file"]) + "',", 1);
+            lines = lines.replace("ann_file=,",
+                                  "ann_file='" + str(self.system_dict["params"]["val_anno_file"]) + "',", 2);
+            lines = lines.replace("ann_file=,",
+                                  "ann_file='" + str(self.system_dict["params"]["val_anno_file"]) + "',", 3);
+            lines = lines.replace("img_prefix=,",
+                                  "img_prefix='" + str(self.system_dict["params"]["train_img_folder"]) + "/',", 1);
+            lines = lines.replace("img_prefix=,",
+                                  "img_prefix='" + str(self.system_dict["params"]["val_img_folder"]) + "/',", 2);
+            lines = lines.replace("img_prefix=,",
+                                  "img_prefix='" + str(self.system_dict["params"]["val_img_folder"]) + "/',", 3);
+            lines = lines.replace("evaluation = dict(interval=",
+                                  "evaluation = dict(interval=" + str(self.system_dict["params"]["val_interval"]));
+            lines = lines.replace("checkpoint_config = dict(interval=",
+                                  "checkpoint_config = dict(interval=" + str(self.system_dict["params"]["val_interval"]));
+            lines = lines.replace("lr=",
+                                  "lr=" + str(self.system_dict["params"]["lr"]));
+            lines = lines.replace("momentum=",
+                                  "momentum=" + str(self.system_dict["params"]["momentum"]));
+            lines = lines.replace("weight_decay=",
+                                  "weight_decay=" + str(self.system_dict["params"]["weight_decay"]));
+            lines = lines.replace("total_epochs =",
+                                  "total_epochs =" + str(self.system_dict["params"]["num_epochs"]));
+            lines = lines.replace("load_from =",
+                                  "load_from = '" + str(self.system_dict["params"]["load_from"]) + "'");
+            lines = lines.replace("classes=,",
+                                  "classes=" + str(self.system_dict["params"]["classes"]) + ",");
+            lines = lines.replace("num_classes=80",
+                                  "num_classes=" + str(len(self.system_dict["params"]["classes"])));
+
+            if(self.system_dict["params"]["num_epochs"] >= 3):
+                steps = [self.system_dict["params"]["num_epochs"]//3, 2*self.system_dict["params"]["num_epochs"]//3];
+            else:
+                steps = [1];
+            lines = lines.replace("step=",
+                                  "step=" + str(steps));
+
+            f = open("config_updated.py", 'w');
+            f.write(lines);
+            f.close();
             
             
             
         
-            
+             
         
     def Train(self):
         self.setup();
