266bdf75479096a91ef2f796fee9bda79b2db5e1
abhi-kumar <abhishek4273@gmail.com>
2020-09-16 18:29:33 +0530

    Resolved error with tf compilation in 12_tf_obj_1 pipeline

--- 12_tf_obj_1/installation/install_cuda10_tensorrt6_part1.sh
+++ 12_tf_obj_1/installation/install_cuda10_tensorrt6_part1.sh
@@ -1,1 +1,5 @@
 cat requirements_cuda10_tensorrt6.txt | xargs -n 1 -L 1 pip install
+
+pip3 install -U --user keras_applications==1.0.8 --no-deps
+
+pip3 install -U --user keras_preprocessing==1.0.8 --no-deps
